27fad0a3670e6380cbbaa90c472624413ca63b10
jeffbabuyo <jefferson@online-edu.com>
2019-07-25 15:18:50 +0800

    added test function

--- src/helpers.php
+++ src/helpers.php
@@ -1,9 +1,9 @@
 <?php
 
 
-if !function_exists('currencyFormat') {
+if ( !function_exists('currencyFormat') ) {
     function currencyFormat()
     {
-        return 'this is currencyFormat';
+        return 'this is coming from your local package symlink in vendor';
     }
 }
