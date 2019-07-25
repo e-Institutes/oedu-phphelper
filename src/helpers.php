ab785fe91a1b2f4fe67dd15bd98fd9582720e837
Yorke Hinds <yhinds@e-institutes.com>
2019-07-25 15:55:25 +0200

    no message

--- src/helpers.php
+++ src/helpers.php
@@ -1,9 +1,10 @@
 <?php
 
-
 if ( !function_exists('currencyFormat') ) {
-    function currencyFormat()
+    function currencyFormat($amount,$currency_format)
     {
-        return 'this is coming from your local package symlink in vendor';
+		$fmt = json_decode(preg_match('~\{.*?\}~', $currency_format, $out) ? $out[0] : '{"d" : 2,"dp" : ".","s" : ","}');
+		$price = preg_replace('/\{.*?\}/', number_format($amount, $fmt->{'d'} , $fmt->{'dp'}, $fmt->{'s'}) , $currency_format);
+        return $price;
     }
 }
