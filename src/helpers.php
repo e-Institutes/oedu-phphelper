e8ba68b1e67410d7cad2b5de4e6114fd7743bd80
Yorke Hinds <yhinds@e-institutes.com>
2019-07-29 17:58:09 +0200

    no message

--- src/helpers.php
+++ src/helpers.php
@@ -4,7 +4,6 @@ if ( !function_exists('currencyFormat') ) {
     function currencyFormat($amount,$currency_format)
     {
 		$fmt = json_decode(preg_match('~\{.*?\}~', $currency_format, $out) ? $out[0] : '{"d" : 2,"dp" : ".","s" : ","}');
-		$price = preg_replace('/\{.*?\}/', number_format($amount, $fmt->{'d'} , $fmt->{'dp'}, $fmt->{'s'}) , $currency_format);
-        return $price;
+        return preg_replace('/\{.*?\}/', number_format($amount, $fmt->{'d'} , $fmt->{'dp'}, $fmt->{'s'}) , $currency_format);
     }
 }
